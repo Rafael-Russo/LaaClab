d2012308de06aa04cc2e135cfa0dbda447360c83
Rafael Russo Soares <rafaelrussosoares@terra.com.br>
2026-08-14 21:58:20 -0300

    chore(flask): dependencias e harness pytest

---
+++ tests/conftest.py
@@ -0,0 +1,5 @@
+"""Fixtures compartilhadas da suíte Flask.
+
+Cresce a cada fatia: aqui entram só as fixtures que não dependem de model
+algum. `db`, `client` e os clientes autenticados chegam nas tasks seguintes.
+"""
